151ca57b6070648316cb31eb90ee706c484deb13
David Maluenda Niubó <dmaluenda@ub.edu>
2022-11-17 17:07:31 +0100

    fixing issues about units, normalization and presentation

--- data/crop.py
+++ data/crop.py
@@ -1,5 +1,7 @@
 
 import sys, os
+
+import matplotlib.pyplot as plt
 import numpy as np
 import imageio
 from glob import glob
@@ -8,12 +10,58 @@ def crop(image, size, center=None):
     """ Crop the image to a square of size size centered on center.
         If certer is None, the center of the image is used.
     """
+    out = np.zeros((size, size))
     y0, x0 = center if center else (image.shape[0]//2, image.shape[1]//2)
     yf, xf = y0 + size//2, x0 + size//2
     yi, xi = y0 - size//2, x0 - size//2
 
-    return image[yi:yf, xi:xf]
+    # print(f"yf={yf}, xf={xf}, yi={yi}, xi={xi} : MAX: {image.shape}")
 
+    out_yi = 0
+    if yi < 0:
+        out_yi = abs(yi)
+        out[0:out_yi, :] = 0
+        yi = 0
+
+    out_xi = 0
+    if xi < 0:
+        out_xi = abs(xi)
+        out[:, 0:out_xi] = 0
+        xi = 0
+
+    out_yf = size
+    if yf > image.shape[0]:
+        out_yf = yf - image.shape[0]
+        out[out_yf:, :] = 0
+        yf = image.shape[0]
+
+    out_xf = size
+    if xf > image.shape[1]:
+        out_xf = xf - image.shape[1]
+        out[:, out_xf:] = 0
+        xf = image.shape[1]
+
+    # print(f"out_yi={out_yi}, out_xi={out_xi}, out_yf={out_yf}, out_xf={out_xf}")
+    out[out_yi:out_yf, out_xi:out_xf] = image[yi:yf, xi:xf]
+    # plt.imshow(out)
+    # plt.show()
+
+    return out
+
+def find_center(image, template):
+    """
+    Find the center of the template in the image.
+    """
+    # Compute the correlation between the template and the image
+    ft_image = np.fft.fft2(image)
+    ft_template = np.fft.fft2(template, s=image.shape)
+    ft_corr = ft_image * np.conj(ft_template)
+    corr = np.fft.ifft2(ft_corr)
+    corr2 = corr*np.conj(corr)
+    # Find the location of the maximum correlation
+    yloc, xloc = np.where(corr2 == corr2.max())
+
+    return yloc[0]+template.shape[0]//2, xloc[0]+template.shape[1]//2
 
 if __name__ == "__main__":
     """ Test the crop function.
@@ -23,24 +71,57 @@ if __name__ == "__main__":
 
     args = sys.argv[1:]
 
-    if len(args) < 2:
-        print("Usage: python crop.py <pattern> <size>")
+    if len(args) < 2 or '-h' in args or '--help' in args:
+        print("Usage: python crop.py <pattern> <size> [--replace|--out <crop_dir>] "
+              "[--tmp <templates dir>] [--verbose|-v]")
         sys.exit(1)
 
     replace = False
-    if args[2] == "--replace":
+    if "--replace" in args:
         replace = True
         args.remove("--replace")
+
+    tmp_dir = None
+    if "--tmp" in args:
+        tmp_dir = args[args.index("--tmp") + 1]
+        args.remove("--tmp")
+        args.remove(tmp_dir)
+        templates = os.listdir(tmp_dir)
+
+    out_dir = None
+    if "--out" in args:
+        out_dir = args[args.index("--out") + 1]
+        args.remove("--out")
+        args.remove(out_dir)
+        os.makedirs(out_dir, exist_ok=True)
+
+    verbose = True if "-v" in args or "--verbose" in args else False
 
     if len(args) > 2:
         print("Warning: ignoring extra arguments")
 
     pattern = args[0]
     size = int(args[1])
-
+    center = None
     for file in glob(pattern):
-        print("Processing file %s" % file)
+        print("Processing file %s" % file) if verbose else None
         image = imageio.imread(file)
-        cropped = crop(image, size)
-        new_fn = file.replace(".png", "_recrop.png") if not replace else file
-        imageio.imwrite(new_fn, cropped)
+        if tmp_dir:
+            tmp_basename = [t for t in templates if
+                            t.startswith(os.path.split(os.path.basename(file)))][0]
+            print(f"Template file {os.path.join(tmp_dir, tmp_basename)}") if verbose else None
+            template = imageio.imread(os.path.join(tmp_dir, tmp_basename))
+            center = find_center(image[:, :800], template)
+            print(f"Center found at {center[::-1]}") if verbose else None
+            if verbose:
+                fig, axs = plt.subplots(1, 2)
+                axs[0].imshow(image)
+                axs[1].imshow(template)
+                plt.show()
+
+        cropped = crop(image, size, center)
+
+        new_fn = file.replace(".png", "_crop.png") if not replace else file
+        if out_dir:
+            new_fn = os.path.join(out_dir, os.path.basename(new_fn))
+        imageio.imwrite(new_fn, cropped.astype(np.uint16))
---
+++ utils.py
@@ -0,0 +1,421 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from mpl_toolkits.axes_grid1 import ImageGrid
+from scipy.fft import fft2, ifft2, fftshift, ifftshift
+from propygator.HighAperture import debye_ricwol
+from propygator.Misc import get_zernike_index, zernike_p
+
+fft = lambda field: fftshift(fft2(ifftshift(field)))
+ifft = lambda spectr: ifftshift(ifft2(fftshift(spectr)))
+
+kind = "radial"
+
+def ricardo_llop(E, NA, L, f, z=0):
+    ny, nx, nc = E.shape
+    Ef = np.zeros((ny, nx, 3), np.complex128)
+
+    y, x = np.mgrid[-ny//2:ny//2, -nx//2:nx//2]
+    y = y/y.max()*L
+    x = x/x.max()*L
+
+    r2 = x*x+y*y
+    sinth2 = r2/f/f
+    mask = sinth2 < NA*NA
+    sinth = np.sqrt(sinth2)*(mask)
+
+    costh = np.sqrt(1-sinth2, where=mask)
+    costh *= mask
+    sqcosth = np.sqrt(costh)
+    phi = np.arctan2(y, x)
+    sinphi = np.sin(phi)
+    cosphi = np.cos(phi)
+
+    # Multiplicació pels vectors unitaris...
+    Ef[:, :, 0] = mask*E[:, :, 0]*(sinphi*sinphi+cosphi*cosphi*costh) +\
+                  mask*E[:, :, 1]*sinphi*cosphi*(costh-1)
+    Ef[:, :, 0][mask] /= sqcosth[mask]
+    Ef[:, :, 1] = mask*E[:, :, 1]*(sinphi*sinphi*costh+cosphi*cosphi) +\
+                  mask*E[:, :, 0]*sinphi*cosphi*(costh-1)*mask
+    Ef[:, :, 1][mask] /= sqcosth[mask]
+    Ef[:, :, 2] = -sinth*(E[:, :, 0]*cosphi + E[:, :, 1]*sinphi)
+    Ef[:, :, 2][mask] /= sqcosth[mask]
+
+    # Propagació
+    if z != 0:
+        H = np.exp(-2j*np.pi*z)*mask
+        Ef[:, :, 0] *= H
+        Ef[:, :, 1] *= H
+        Ef[:, :, 2] *= H
+    Ef[:, :, 0] = fft(Ef[:, :, 0])
+    Ef[:, :, 1] = fft(Ef[:, :, 1])
+    Ef[:, :, 2] = fft(Ef[:, :, 2])
+    return Ef
+
+def get_z_component(Ex, Ey, p_size, lamb=520e-3, z=0):
+    Ax = fft(Ex)
+    Ay = fft(Ey)
+    ny, nx = Ex.shape
+
+    y, x = np.mgrid[-ny//2:ny//2, -nx//2:nx//2]
+
+    alpha = x/x.max()/p_size*lamb*0.5
+    beta = y/y.max()/p_size*lamb*0.5
+
+    gamma = np.sqrt(1+0j-alpha*alpha-beta*beta)
+    Az = (alpha*Ax+beta*Ay)/gamma
+
+
+    # Propaguem si s'escau
+    if z > 0:
+        H = np.exp(-2j*np.pi*gamma*z)
+        H[alpha*alpha+beta*beta >= 1] = 0
+        Az *= H
+        Ax *= H
+        Ay *= H
+        Ex = ifft(Ax)
+        Ey = ifft(Ay)
+
+    Ez = ifft(Az)
+
+    return Ex, Ey, Ez
+
+
+def get_theoretical_field(kind, NA=0.7, lamb=520e-3, n=256):
+    """ kind = "radial", "circular" or "lineal"
+    """
+    f = 5/lamb
+    E = np.zeros((n, n, 2), dtype=np.complex128)
+    y, x = np.mgrid[-n//2:n//2, -n//2:n//2]
+    phi = np.arctan2(y, x)
+    if kind == "radial":
+        E[:, :, 0] = np.cos(phi)
+        E[:, :, 1] = np.sin(phi)
+    elif kind == "circular":
+        E[:, :, 0] = 1
+        E[:, :, 1] = 1j
+    elif kind == "lineal":
+        E[:, :, 0] = 0
+        E[:, :, 1] = 1
+
+    Lf = 16
+    L = n*f/4/Lf
+
+    # Camp focal
+    return ricardo_llop(E, NA, L, f)
+
+
+def print_fig(msg, fig_num):
+    fig_num += 1
+    print(f"Figure {fig_num}: {msg}")
+    return fig_num
+
+
+def plot_raw_trans_field(Ex, Ey, label='', fig_num=0, cmap_abs='jet', cmap_ph='hsv'):
+    fig, ax = plt.subplots(2, 2, constrained_layout=True, figsize=(10,10))
+    ax[0, 0].imshow(np.abs(Ex), cmap=cmap_abs)
+    ax[0, 0].set_title(r"$|Ex|$")
+    ax[1, 0].imshow(np.abs(Ey), cmap=cmap_abs)
+    ax[1, 0].set_title(r"$|Ey|$")
+    ax[0, 1].imshow(np.angle(Ex), cmap=cmap_ph)
+    ax[0, 1].set_title(r"$\phi_x$")
+    ax[1, 1].imshow(np.angle(Ey), cmap=cmap_ph)
+    ax[1, 1].set_title(r"$\phi_y$")
+    plt.show()
+    title = f"(Pol. {label}) The raw data of the retrieved transversal component."
+    return print_fig(title, fig_num)
+
+
+def plot_raw_long_field(Ez, label='', fig_num=0, cmap_abs='jet', cmap_ph='hsv'):
+    fig, ax = plt.subplots(1, 2, constrained_layout=True, figsize=(10,20))
+    ax[0].imshow(np.abs(Ez), cmap=cmap_abs)
+    ax[0].set_title(r"$|Ez|$")
+    ax[1].imshow(np.angle(Ez), cmap=cmap_ph)
+    ax[1].set_title(r"$\phi_z$")
+    plt.show()
+    title = f"(Pol. {label}) The raw data of the retrieved longitudinal component."
+    return print_fig(title, fig_num)
+
+
+def plot_fields(Ex, Ey, Ez, fig_num=0, trim=None, label="", verbose=1,
+                pixel_size=1, lamb=520e-3, ticks_step=1):
+    """
+
+    :param Ex:
+    :param Ey:
+    :param Ez:
+    :param fig_num:
+    :param trim:
+    :param label:
+    :param verbose:
+    :param pixel_size:
+    :param lamb:
+    :param ticks_step: in lambdas
+    :return:
+    """
+    ntrim = -trim if trim else None
+    lims = Ex[trim:ntrim, trim:ntrim].shape
+
+    extension = lims[0] * pixel_size / lamb  # window size in microns
+    half_side = extension / 2
+    extent = [-half_side, half_side, -half_side, half_side]
+
+    ticks = [0]
+    for tt in range(int(extension / ticks_step / 2)):
+        tt1 = tt + 1
+        ticks.append(tt1 * ticks_step)
+        ticks.insert(0, -tt1 * ticks_step)
+
+    fig = plt.figure(figsize=(20, 40))
+    axs = ImageGrid(fig, 111,
+                    nrows_ncols=(2, 4),
+                    axes_pad=0.4,
+                    cbar_location="right",
+                    cbar_mode="edge",
+                    cbar_size="5%",
+                    cbar_pad=0.2
+                    )
+
+    Ax = np.abs(Ex[trim:ntrim, trim:ntrim])
+    Ay = np.abs(Ey[trim:ntrim, trim:ntrim])
+    Az = np.abs(Ez[trim:ntrim, trim:ntrim])
+    phx = np.angle(Ex[trim:ntrim, trim:ntrim])
+    phy = np.angle(Ey[trim:ntrim, trim:ntrim])
+    phz = np.angle(Ez[trim:ntrim, trim:ntrim])
+
+    I = Ax ** 2 + Ay ** 2
+
+    # phx = np.mod(phx + np.pi, 2*np.pi) - np.pi  # range [-pi, pi], 2*np.pi)
+    # phy = np.mod(phy + np.pi, 2*np.pi) - np.pi  # range [-pi, pi], 2*np.pi)
+    ph = np.mod(phy - phx + np.pi, 2 * np.pi) - np.pi  # range [-pi, pi]
+
+    my_greens = np.zeros((256, 4))
+    my_greens[:, 1] = np.linspace(0, 1, 256)
+    my_greens[:, 3] = 1
+    cmap_amps = ListedColormap(my_greens)
+    cmap_phs = 'twilight_shifted'
+
+    normalize = lambda a: a / a.max()  # (a-a.min())/(a.max()-a.min())
+
+    if verbose > 1:
+        get_title = lambda name, im: (fr'${name} ; '
+                                      fr'vmin={im.min():.2g} ; '
+                                      fr'vmax={im.max():.2g}$')
+        fs = 14
+    else:
+        get_title = lambda name, im: (fr'${name}$')
+        fs = 20
+
+    for idx, ax in enumerate(axs):
+        ax.set_aspect('equal')
+        ax.set_xlabel(r'$x \, / \, \lambda$')
+        ax.set_ylabel(r'$y \, / \, \lambda$')
+
+        if idx == 0:
+            im = ax.imshow(normalize(Ax), cmap=cmap_amps,
+                           vmin=0, vmax=1, extent=extent)
+            ax.set_title(get_title('|E_x|', Ax), fontsize=fs)
+        elif idx == 1:
+            im = ax.imshow(normalize(Ay), cmap=cmap_amps,
+                           vmin=0, vmax=1, extent=extent)
+            ax.set_title(get_title('|E_y|', Ay), fontsize=fs)
+        elif idx == 2:
+            im = ax.imshow(normalize(I), cmap=cmap_amps,
+                           vmin=0, vmax=1, extent=extent)
+            ax.set_title(get_title('|E_x|^2+|E_y|^2', I), fontsize=fs)
+        elif idx == 3:
+            im = ax.imshow(normalize(Az), cmap=cmap_amps,
+                           vmin=0, vmax=1, extent=extent)
+            ax.set_title(get_title('|E_z|', Az), fontsize=fs)
+        elif idx == 4:
+            im = ax.imshow(phx, cmap=cmap_phs,
+                           vmin=-np.pi, vmax=np.pi, extent=extent)
+            ax.set_title(r'$\phi_x$', fontsize=fs)
+        elif idx == 5:
+            im = ax.imshow(phy, cmap=cmap_phs,
+                           vmin=-np.pi, vmax=np.pi, extent=extent)
+            ax.set_title(r'$\phi_y$', fontsize=fs)
+        elif idx == 6:
+            im = ax.imshow(ph, cmap=cmap_phs,
+                           vmin=-np.pi, vmax=np.pi, extent=extent)
+            ax.set_title(r'$\phi_y-\phi_x$', fontsize=fs)
+        elif idx == 7:
+            im = ax.imshow(phz, cmap=cmap_phs,
+                           vmin=-np.pi, vmax=np.pi, extent=extent)
+            ax.set_title(r'$\phi_z$', fontsize=fs)
+
+        ax.set_xticks(ticks)
+        ax.set_yticks(ticks)
+        if idx == 5:
+            cbar = axs.cbar_axes[1].colorbar(im,  # FIXME: The line below is not working
+            # cbar = fig.colorbar(im, ax=ax, cax=axs.cbar_axes[idx], orientation='vertical', shrink=0.5,
+                                             ticks=[-np.pi, -np.pi / 2, 0, np.pi / 2,
+                                                    np.pi])
+            cbar.ax.set_yticklabels(
+                [r'$-\pi$', r'$-\pi/2$', r'$0$', r'$\pi/2$', r'$\pi$'])
+        elif idx == 2:
+            cbar = fig.colorbar(im, ax=ax, cax=axs.cbar_axes[0], orientation='vertical',
+                                shrink=0.5,
+                                ticks=[0, 1])
+            cbar.ax.set_yticklabels([r'0', r'1'])
+
+    plt.show()
+    return print_fig(f"(Pol. {label}) Field in the focal plane.", fig_num)
+
+
+def plot_3D_stokes(experimental_s, theoric_s, label, pixel_size=1, lamb=520e-3,
+                   trim=None, fig_num=0):
+    ntrim = -trim if trim else None
+    lims = experimental_s[0][trim:ntrim, trim:ntrim].shape
+
+    ticks_step = 1  # lambda
+
+    extension = lims[0] * pixel_size / lamb  # window size in microns
+    half_side = extension / 2
+    extent = [-half_side, half_side, -half_side, half_side]
+
+    ticks = [0]
+    for tt in range(int(extension / ticks_step / 2)):
+        tt1 = tt + 1
+        ticks.append(tt1 * ticks_step)
+        ticks.insert(0, -tt1 * ticks_step)
+
+    fig = plt.figure(figsize=(20, 30))
+    axs = ImageGrid(fig, 111,
+                    nrows_ncols=(2, 3),
+                    axes_pad=0.4,
+                    cbar_location="right",
+                    cbar_mode="single",
+                    cbar_size="5%",
+                    cbar_pad=0.2
+                    )
+
+    titles = [fr'$S_{i}$' for i in range(4)]
+    titles.insert(2, r'$S_1^{num}$')
+    titles.insert(5, r'$S_3^{num}$')
+
+    # teo_max = theoric_s[:,:,0].max()
+    # exp_max = experimental_s[:, :, 0].max()
+    # stokes = [experimental_s[:,:,i] / experimental_s[:,:,i].max() for i in range(4)]
+    # stokes.insert(2, theoric_s[:,:,1]/theoric_s[:,:,1].max())
+    # stokes.insert(5, theoric_s[:,:,3]/theoric_s[:,:,3].max())
+    # stokes[3] = np.zeros_like(experimental_s[:,:,2])
+
+    stokes = [(experimental_s[:, :, i]) for i in range(4)]
+    stokes.insert(2, (theoric_s[:, :, 1]))
+    stokes.insert(5, (theoric_s[:, :, 3]))
+    stokes[3] = np.zeros_like(experimental_s[:, :, 2])  # all ZEROS generates NaN
+
+    for idx, ax in enumerate(axs):
+        ax.set_aspect('equal')
+        ax.set_xlabel(r'$x \, / \, \lambda$')
+        ax.set_ylabel(r'$x \, / \, \lambda$')
+
+        im = ax.imshow(stokes[idx][trim:ntrim, trim:ntrim],
+                       cmap='Reds', vmin=0, vmax=1, extent=extent)  #
+        ax.set_title(titles[idx])
+        ax.set_xticks(ticks)
+        ax.set_yticks(ticks)
+
+    plt.colorbar(im, cax=axs.cbar_axes[0], ticks=[0, 1])
+    plt.show()
+
+    return print_fig(f"(Pol. {label}) 3D Stokes in the PQ-basis and "
+                     f"the numerical calculation for comparison purposes.", fig_num)
+
+if __name__ == "__main__":
+    n = 512
+    NA = 0.35
+    lamb = 520e-6
+    f = 5/lamb
+    E = np.zeros((n, n, 2), dtype=np.complex128)
+    y, x = np.mgrid[-n//2:n//2, -n//2:n//2]
+    x = x+3
+    y = y+3
+    phi = np.arctan2(y, x)
+    if kind == "radial":
+        E[:, :, 0] = np.cos(phi)
+        E[:, :, 1] = np.sin(phi)
+    elif kind == "circular":
+        E[:, :, 0] = 1
+        E[:, :, 1] = -1j
+
+    Lf = 16
+    L = n*f/4/Lf
+
+    # Càlcul Zernikes...
+    rho = np.sqrt(x*x+y*y)/n*L/f
+    rho[rho> NA] = 0
+    rho/=rho.max()
+    #
+    coeffs = {0:0,
+              1 :0e-2,
+              2 :0e-2,
+              3 :0e-2,
+              5 :0e-2,
+              6 :-0e-2,
+              7 :0.0e-1,
+              8 :0.0e-2,
+              9 :0.0e-2,
+              12:-0e-1,
+             }
+    idx = get_zernike_index("OSA")
+    first = True
+    for key in coeffs.keys():
+        value = coeffs[key]
+        n, m = idx[key]
+        if first:
+            R = value*zernike_p(rho, phi, n, m)
+            first = False
+        else:
+            R += value*zernike_p(rho, phi, n, m)
+    E[:, :, 0] *= np.exp(2j*np.pi*R)
+    E[:, :, 1] *= np.exp(2j*np.pi*R)
+    # Camp focal
+    Ef = ricardo_llop(E, NA, L, f)
+
+    # Stokes en coordenades estranyes
+    import import_ipynb
+    from Report import compute_3D_stokes
+    #Ef[:, :, 0] = np.roll(Ef[:, :, 0], (4, -4), axis=(0, 1))
+    s = compute_3D_stokes(Ef[:, :, 0], Ef[:, :, 1], Ef[:, :, 2])
+    s /= s.max()
+
+    I = np.real(np.conj(Ef)*Ef)
+    fig, ax = plt.subplots(2, 2, constrained_layout=True)
+    fig.suptitle(f"Irradiances in cartesian basis, {kind}")
+
+    extent = (-Lf, Lf, -Lf, Lf)
+    extent = [l*lamb*1000 for l in extent]
+    cmap="gray"
+    ax[0, 0].imshow(I[:, :, 0], cmap=cmap, extent=extent)
+    ax[0, 0].set_title("$I_x$")
+    ax[0, 1].imshow(I[:, :, 1], cmap=cmap, extent=extent)
+    ax[0, 1].set_title("$I_y$")
+    ax[1, 0].imshow(I[:, :, 2], cmap=cmap, extent=extent)
+    ax[1, 0].set_title("$I_z$")
+    ax[1, 1].imshow(np.sum(I, axis=-1), cmap=cmap, extent=extent)
+    ax[1, 1].set_title("$I_t$")
+
+    fig2, ax2 = plt.subplots(2, 2, constrained_layout=True, figsize=(8,8))
+    fig2.suptitle(f"Stokes in strange basis, {kind}")
+    ax2[0, 0].imshow(s[:, :, 0], cmap=cmap, extent=extent)
+    ax2[0, 0].set_title("$S_0$")
+    ax2[0, 1].imshow(s[:, :, 1], cmap="bwr", extent=extent, vmin=-1, vmax=1)
+    ax2[0, 1].set_title("$s_1$")
+    ax2[1, 0].imshow(s[:, :, 2], cmap="bwr", extent=extent, vmin=-1, vmax=1)
+    ax2[1, 0].set_title("$s_2$")
+    ax2[1, 1].imshow(s[:, :, 3], cmap="bwr", extent=extent, vmin=-1, vmax=1)
+    ax2[1, 1].set_title("$s_3$")
+
+    ax2[0, 0].set_xlabel("x ($\mathrm{\mu m}$)")
+    ax2[0, 0].set_ylabel("y ($\mathrm{\mu m}$)")
+    ax2[0, 1].set_xlabel("x ($\mathrm{\mu m}$)")
+    ax2[0, 1].set_ylabel("y ($\mathrm{\mu m}$)")
+    ax2[1, 0].set_xlabel("x ($\mathrm{\mu m}$)")
+    ax2[1, 0].set_ylabel("y ($\mathrm{\mu m}$)")
+    ax2[1, 1].set_xlabel("x ($\mathrm{\mu m}$)")
+    ax2[1, 1].set_ylabel("y ($\mathrm{\mu m}$)")
+
+    fig2.savefig(f"{kind}.png", bbox_inches="tight", dpi=200)
+    plt.show()
